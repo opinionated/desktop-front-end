3ec9a9391b822c5c101d88aa645748a0b7cbaac6
Theodore Rice <theo.rice1996@gmail.com>
2015-12-04 14:38:05 -0500

    Made the header clickable to go back to homepage, just add anchor tag in html

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$('#footer').append('<ul id="footer_bar">\
 			<li><a href="index.html" class="btn_ft">Contact</a></li>\
+			<li><a href="index.html" class="btn_ft">About</a></li>\
+			<li><a href="http://cdn.playbuzz.com/cdn/67cb2522-0009-40cb-8dda-bf10931695ac/6a5c5e61-bdbb-40cf-ade5-f76e3e90dea4.jpg" class="btn_ft">Bob Dylan</a></li>\
 			<li><a href="index.html" class="btn_ft">Donate</a></li>\
 			<li><a href="index.html" class="btn_ft">Privacy</a></li>\
 			<li><a href="index.html" class="btn_ft">Terms of Service</a></li>\
