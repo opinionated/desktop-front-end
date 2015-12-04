7f978fc1368b3125eef0a5874b63f2935465365f
Theodore Rice <theo.rice1996@gmail.com>
2015-12-04 03:04:47 -0500

    added a footer that can be added similar to header

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#footer').append('<ul id="footer_bar">\
+			<li><a href="index.html" class="btn_ft">Contact</a></li>\
+			<li><a href="index.html" class="btn_ft">Donate</a></li>\
+			<li><a href="index.html" class="btn_ft">Privacy</a></li>\
+			<li><a href="index.html" class="btn_ft">Terms of Service</a></li>\
+		</ul>');
+});
