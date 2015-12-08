ad7d790bb8c3b6c6cb4da27f1c3fd5ee1bc94cf6
Jon Patsenker <jonathan@patsenker.com>
2015-12-08 12:38:03 -0500

    templates fixing

--- js/get_article_body.js
+++ js/get_article_body.js
@@ -20,6 +20,18 @@ $(document).ready(function() {
 		// 	);
 		// }
 
+		if ($('#article-template') == "right") {
+			$('#article-photo').append('<img src="' + article.article.image + '">' + <br>);
+			$('#article-photo-caption').append(article.article.imageCaption);
+
+			$('#article-body').append('<h2>'+ article.article.title +'</h2>' +
+									'<p class="author">' + article.article.author + '</p>' +
+									'<p class="date">' + article.article.date + '</p>' +
+									'<p class="description">' + article.article.description + '</p>' +
+									'<p>' + article.article.body + '</p>'
+			);
+			#('#article-photo-wrapper').toggleClass("right");
+		}
 
 
 
