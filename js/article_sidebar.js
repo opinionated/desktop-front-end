8c5a057848a105f0a19461d9df0cac090bb93baa
Jon Patsenker <jonathan@patsenker.com>
2015-12-08 12:01:40 -0500

    main body close sidebar

--- js/article_sidebar.js
+++ js/article_sidebar.js
@@ -3,3 +3,8 @@ $("#menu-toggle").click(function(e) {
 	$("#wrapper").toggleClass("toggled");
 	$("#sidebar_toggle_wrapper").toggleClass("toggled");
 });
+$("#article-wrapper").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+	$("#sidebar_toggle_wrapper").toggleClass("toggled");
+});
