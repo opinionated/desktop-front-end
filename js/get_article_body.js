442e75e1f259ae121bca58d17e52e98aa678b8e4
TheoRice <theo.rice1996@gmail.com>
2015-12-08 14:08:25 -0500

    centered image and caption for top

--- js/get_article_body.js
+++ js/get_article_body.js
@@ -7,12 +7,13 @@ $(document).ready(function() {
 
 	pullFile("../resources/exJson/" + article_name, function(article){
 		if (article.article.template == 'top') {
-			$('#article-header').append('<h2 style="clear:both;">'+ article.article.title +'</h2>' +
-									'<p class="author">' + article.article.author + '</p>' +
-									'<p class="date">' + article.article.date + '</p>')
-			$('#article-photo').append('<img src="' + article.article.image + '">');
+			$('#article-header').append('<h2>'+ article.article.title +'</h2>' +
+									'<p class="author" style ="float: left;">' + article.article.author + '</p>' +
+									'<p class="date" style = "clear: both";>' + article.article.date + '</p>')
+			$('#article-photo-wrapper').addClass("top_template");
+			$('#article-photo').append('<img src="' + article.article.image + '"">');
 			$('#article-photo-caption').append(article.article.imageCaption);
-			$('#article-body').append('<p class="description">' + article.article.description + '</p>' +
+			$('#article-body').append('<br><p class="description">' + article.article.description + '</p>' +
 									'<p>' + article.article.body + '</p>'
 			);
 		}
