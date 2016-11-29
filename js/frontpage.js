ac17175ab9ef88356b172424c5be307e78af0316
TheoRice <theo.rice1996@gmail.com>
2016-11-29 16:27:26 -0500

    fixed index bug, now %3=2 works

--- js/frontpage.js
+++ js/frontpage.js
@@ -49,22 +49,19 @@ $(document).ready(function() {
         }
         else if (jsonFArticles.length % 3 == 1) {
             if (jsonFArticles[0].image != "") {
-                $("#everythingElse").prepend('<div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
+                $("#everythingElse").prepend('<a href="html/article_page.html?article=' + jsonFArticles[0].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
                     '<p class="author">' + jsonFArticles[0].author + ' | ' + '<font color="grey"> ' + jsonFArticles[0].date + '</font></p>' +
                     '<p class="body"> <img style="float:right; margin: 5px 5px 5px 15px;"" src="' + jsonFArticles[0].image + '" alt="" height="225px">' +
-                        jsonFArticles[0].description + '</p> <a href="html/article_page.html?article=' + jsonFArticles[0].file + '" style="color: #333; text-decoration: none;"> </a>'
+                        jsonFArticles[0].description + '</p> </a> </div>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
             else {
-                $("#everythingElse").prepend('<a href=index.html style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
+                $("#everythingElse").prepend('<a href="html/article_page.html?article=' + jsonFArticles[0].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
                     '<p class="author">' + jsonFArticles[0].author + ' | ' + '<font color="grey"> ' + jsonFArticles[0].date + '</font></p>' +
-                    '<p>' + jsonFArticles[0].description + '</p> </a>'
+                    '<p class="body">' + jsonFArticles[0].description +
+                    '</p> </a> </div>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
 
@@ -80,7 +77,6 @@ $(document).ready(function() {
                             '<p class="author">' + jsonFArticles[i].author + ' | ' + '<font color="grey">' + jsonFArticles[i].date + '</font>' + '</p>' + '<p>' + jsonFArticles[i].description + '</p>' +
                             '</div> </a>')
                     }
-
                 }
                 else if (i%3==1) {
                     if (jsonFArticles[i].image != "") {
@@ -111,27 +107,23 @@ $(document).ready(function() {
         }
         else {
             if (jsonFArticles[0].image != "") {
-                $("#everythingElse").prepend('<a href=index.html style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
+                $("#everythingElse").prepend('<a href="html/article_page.html?article=' + jsonFArticles[0].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
                     '<p class="author">' + jsonFArticles[0].author + ' | ' + '<font color="grey"> ' + jsonFArticles[0].date + '</font></p>' +
-                    '<p> <img style="float:center;"style="float:left; margin: 5px 15px 5px 0px;"src="' + jsonFArticles[i].image + '" alt="" width="140px">' + jsonFArticles[0].description + '</p>' +
-                    '<a href="html/article_page.html?article=' + jsonFArticles[0].file + '"></a>'
+                    '<p class="body"> <img style="float:right; margin: 5px 5px 5px 15px;"" src="' + jsonFArticles[0].image + '" alt="" height="225px">' +
+                        jsonFArticles[0].description + '</p> </a> </div>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
             else {
-                $("#everythingElse").prepend('<a href=index.html style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
+                $("#everythingElse").prepend('<a href="html/article_page.html?article=' + jsonFArticles[0].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[0].title + '</h2>' +
                     '<p class="author">' + jsonFArticles[0].author + ' | ' + '<font color="grey"> ' + jsonFArticles[0].date + '</font></p>' +
-                    '<p>' + jsonFArticles[0].description + '</p>' +
-                    '<a href="html/article_page.html?article=' + jsonFArticles[0].file + '"></a>'
+                    '<p class="body">' + jsonFArticles[0].description +
+                    '</p> </a> </div>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
 
-            for (var i=1; i < jsonFArticles.length; i++) {
+            for (var i=1; i < jsonFArticles.length-1; i++) {
                 if (i % 3 == 0) {
                     if (jsonFArticles[i].image != "") {
                         $('#column1').append('<a href="html/article_page.html?article=' + jsonFArticles[i].file + '" style="color: #333; text-decoration: none;"> <div class="article"><h4>' + jsonFArticles[i].title + '</h4>' +
@@ -145,7 +137,7 @@ $(document).ready(function() {
                     }
 
                 }
-                else if (i%3==1) {
+                else if (i % 3 == 1) {
                     if (jsonFArticles[i].image != "") {
                         $('#column2').append('<a href="html/article_page.html?article=' + jsonFArticles[i].file + '" style="color: #333; text-decoration: none;"> <div class="article"><h4>' + jsonFArticles[i].title + '</h4>' +
                             '<p class="author">' + jsonFArticles[i].author + ' | ' + '<font color="grey">' + jsonFArticles[i].date + '</font>' + '</p>' + '<p> <img style="float:left; margin: 5px 15px 5px 0px;" src="' + jsonFArticles[i].image + '" alt="" width="140px">' + jsonFArticles[i].description + '</p>' +
@@ -170,25 +162,20 @@ $(document).ready(function() {
                     }
                 }
             }
-
-            if (jsonFArticles[jsonFArticles.length].image != "") {
-                $("#everythingElse").append('<a href=index.html style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[jsonFArticles.length].title + '</h2>' +
-                    '<p class="author">' + jsonFArticles[jsonFArticles.length].author + ' | ' + '<font color="grey"> ' + jsonFArticles[jsonFArticles.length].date + '</font></p>' +
-                    '<p> <img style="float:center;"style="float:left; margin: 5px 15px 5px 0px;" src="' + jsonFArticles[jsonFArticles.length].image + '" alt="" width="140px">' + jsonFArticles[jsonFArticles.length].description + '</p>' +
-                    '<a href="html/article_page.html?article=' + jsonFArticles[jsonFArticles.length].file + '"></a>'
+            if (jsonFArticles[jsonFArticles.length-1].image != "") {
+                $("#everythingElse").append('<a href="html/article_page.html?article=' + jsonFArticles[jsonFArticles.length-1].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[jsonFArticles.length-1].title + '</h2>' +
+                    '<p class="author">' + jsonFArticles[jsonFArticles.length-1].author + ' | ' + '<font color="grey"> ' + jsonFArticles[jsonFArticles.length-1].date + '</font></p>' +
+                    '<p> <img style="float:center;"style="float:left; margin: 5px 15px 5px 0px;" src="' + jsonFArticles[jsonFArticles.length-1].image + '" alt="" width="140px">' + jsonFArticles[jsonFArticles.length-1].description + '</p>' +
+                    '</a>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
             else {
-                $("#everythingElse").append('<a href=index.html style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[jsonFArticles.length].title + '</h2>' +
-                    '<p class="author">' + jsonFArticles[jsonFArticles.length].author + ' | ' + '<font color="grey"> ' + jsonFArticles[jsonFArticles.length].date + '</font></p>' +
-                    '<p>' + jsonFArticles[jsonFArticles.length].description + '</p>' +
-                    '<a href="html/article_page.html?article=' + jsonFArticles[jsonFArticles.length].file + '"></a>'
+                $("#everythingElse").append('<a href="html/article_page.html?article=' + jsonFArticles[jsonFArticles.length-1].file + '" style="color: #333; text-decoration: none;"> <div class="jumbotron"><h2>' + jsonFArticles[jsonFArticles.length-1].title + '</h2>' +
+                    '<p class="author">' + jsonFArticles[jsonFArticles.length-1].author + ' | ' + '<font color="grey"> ' + jsonFArticles[jsonFArticles.length-1].date + '</font></p>' +
+                    '<p>' + jsonFArticles[jsonFArticles.length-1].description + '</p>' +
+                    '</a>'
                 ).click(function() {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
                 });
             }
         }
